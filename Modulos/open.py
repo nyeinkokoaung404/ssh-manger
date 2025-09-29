d2a12f0f3878bcb0dd5c0b0ae05fc86a4786bd7b
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2025-09-29 12:13:32 +0630

    Update open.py

--- Modulos/open.py
+++ Modulos/open.py
@@ -12,7 +12,7 @@ except:
 PASS = ''
 BUFLEN = 8196 * 8
 TIMEOUT = 60
-MSG = '🐉ㅤDRAGON VPS MANAGERㅤ🐉'
+MSG = '🌺ㅤ404 VPS MANAGERㅤ🌺'
 DEFAULT_HOST = '0.0.0.0:1194'
 RESPONSE = "HTTP/1.1 200 " + str(MSG) + "\r\n\r\n"
 
@@ -226,7 +226,7 @@ def main(host=IP, port=PORT):
     print "\033[0;34m━"*8,"\033[1;32m PROXY SOCKS","\033[0;34m━"*8,"\n"
     print "\033[1;33mIP:\033[1;32m " + IP
     print "\033[1;33mPORTA:\033[1;32m " + str(PORT) + "\n"
-    print "\033[0;34m━"*10,"\033[1;32m 🐉ㅤDRAGON VPS MANAGERㅤ🐉","\033[0;34m━\033[1;37m"*11,"\n"
+    print "\033[0;34m━"*10,"\033[1;32m 🌺ㅤ404 VPS MANAGERㅤ🌺","\033[0;34m━\033[1;37m"*11,"\n"
     server = Server(IP, PORT)
     server.start()
     while True:
